b0a6297b09ac17c5edc3c80afc71a63769fb1051
thi-tr <thi.t2103@gmail.com>
2026-02-17 11:41:50 -0500

    Added static route and looped image to index page

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express(); //Calling express as a function sets up server
 
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 app.get('/',(req, res)=>{
     console.log('Here');
     res.render('index', {userName:'Thi'});
