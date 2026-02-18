ba050ffd08b363d3ade60d315b0afaae643b0f56
thi-tr <thi.t2103@gmail.com>
2026-02-18 12:17:49 -0500

    Created word finding function and show it on the page

--- routes/words.js
+++ routes/words.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const router = express.Router();
-
-  
+const {readFile, writeFile} = require('fs').promises; //Destructuring
 
 router.get('/', (req, res)=>{
     res.send('Word Homepage');
-
 });
 
-router.get('/wotd', (req, res)=>{
-    res.send('Word of the Day Page');
+router.get('/wotd', async (req, res)=>{
+    let wordArray = await getWordFromDictionary();
+    let [word, part, definition] = wordArray;
+    res.render('wotd', {word:word, part:part, definition:definition});
 });
+
+let getWordFromDictionary = async ()=>{
+    try{
+        const data = await readFile('resources/allwords.txt', 'utf8');
+        let lines = data.split('\n');
+        let randomNumber = parseInt(Math.random()*lines.length);
+        let randomLine = lines[randomNumber];
+        let wordArray = randomLine.split('\t');
+        console.log(wordArray);
+        return wordArray;
+    } catch(err){
+        console.log("there was an error reading the file:", err)
+    }
+};
 
 module.exports = router;
