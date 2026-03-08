af9dae9d7d89faea31ab80fdcd85614d4ccf739d
thi-tr <thi.t2103@gmail.com>
2026-03-08 16:58:01 -0400

    Finish word of the day hw (allwords)

--- routes/words.js
+++ routes/words.js
@@ -11,9 +11,10 @@ router.get('/wotd', async (req, res)=>{
     let [word, part, definition] = wordArray;
     res.render('wotd', {word:word, part:part, definition:definition});
 });
-router.get('allwords', (req,res)=>{
 
-
+router.get('/allwords', async (req,res)=>{
+    let allWordArray = await getAllWordsFromDictionary();
+    res.render('allwords', {word: allWordArray});
 });
 
 let getWordFromDictionary = async ()=>{
@@ -26,7 +27,25 @@ let getWordFromDictionary = async ()=>{
         console.log(wordArray);
         return wordArray;
     } catch(err){
-        console.log("there was an error reading the file:", err)
+        console.log("There was an error reading the file:", err);
+    }
+};
+
+let getAllWordsFromDictionary = async ()=>{
+    try{
+        const data = await readFile('resources/allwords.txt', 'utf8');
+        let lines = data.split('\n');
+
+        //allWordArray to prevent reference error
+        let allWordArray = [];
+        for(let i = 0; i < lines.length; i++){
+            let wordArray = lines[i].split('\t');
+            console.log(wordArray);
+            allWordArray.push(wordArray);
+        }
+        return allWordArray;
+    } catch(err){
+        console.log("There was an error reading the file:", err);
     }
 };
 
